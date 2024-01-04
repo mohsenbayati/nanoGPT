65c3145a4e3dc325995d079a75397cf2d5a37e7f
User <yingyee0111@gmail.com>
2024-01-04 04:43:58 -0800

    custom finetuning

---
+++ config/finetune_custom.py
@@ -0,0 +1,22 @@
+import time
+
+out_dir = 'out-custom'
+eval_interval = 5
+eval_iters = 40
+wandb_log = False # feel free to turn on
+wandb_project = 'custom'
+wandb_run_name = 'ft-' + str(time.time())
+
+dataset = 'custom'
+init_from = 'gpt2-xl' # this is the largest GPT-2 model
+
+# only save checkpoints if the validation loss improves
+always_save_checkpoint = False
+
+batch_size = 1
+gradient_accumulation_steps = 32
+max_iters = 20
+
+# finetune at constant LR
+learning_rate = 3e-5
+decay_lr = False
